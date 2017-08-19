608daae0661251dfc51704ca20a63fd5c5fb47b0
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2017-08-19 11:51:12 +1000

    Add screen template layout flow thing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router, NavigationEnd} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  frequencies = [
+    {value: 'weekly-0', viewValue: 'Weekly'},
+    {value: 'fortnightly-1', viewValue: 'Fortnightly'},
+    {value: 'monthly-2', viewValue: 'Monthly'}
+  ];
+
+  value = 50;
+  bufferValue = 75;
+
+  constructor(private router: Router) {
+    router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = router.parseUrl(router.url);
+        if (tree.fragment) {
+          // you can use DomAdapter
+          const element = document.querySelector("#" + tree.fragment);
+          if (element) { element.scrollIntoView(element); }
+        }
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule, MdInputModule, MdSelectModule, MdProgressBarModule, MdToolbarModule } from '@angular/material';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: AppComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule, MdCheckboxModule, MdInputModule, MdSelectModule, MdProgressBarModule, MdToolbarModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
